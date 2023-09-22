61ea8aa1b953ef689d2d90ad885e6d0c0b8d5889
Bernardo Pinto <bernardodacostapinto@gmail.com>
2023-09-22 13:26:17 +0100

    Lab1 completed

--- Lab1/Ex1_5/AnyCityForecast/src/main/java/pt/ua/deti/WeatherStarter.java
+++ Lab1/Ex1_5/AnyCityForecast/src/main/java/pt/ua/deti/WeatherStarter.java
@@ -2,31 +2,40 @@ package pt.ua.deti;
 
 
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class WeatherStarter {
 
-    public static void  main(String[] args ) {
 
-        try {
-            int cityID = Integer.parseInt(System.getenv("CITY_ID")); //para usar var de ambiente par ao docker
+        public static void main(String[] args) {
+            Random random = new Random();
+            int minCityID = 1000000;
+            int maxCityID = 9999999;
 
-            IpmaCityForecast forecast = consume.getForecast(consume.CreateInterface(consume.getRetrofit()), cityID);
+            try {
+                while (true) {
+                    int cityID = random.nextInt(maxCityID - minCityID + 1) + minCityID;
+                    IpmaCityForecast forecast = consume.getForecast(consume.CreateInterface(consume.getRetrofit()), cityID);
 
-            if (forecast != null) {
-                var firstDay = forecast.getData().listIterator().next();
+                    if (forecast != null) {
+                        var firstDay = forecast.getData().listIterator().next();
 
-                System.out.printf( "max temp for %s is %4.1f %n",
-                        firstDay.getForecastDate(),
-                        Double.parseDouble(firstDay.getTMax()));
+                        System.out.printf("max temp for %s in city %d is %4.1f %n",
+                                firstDay.getForecastDate(),
+                                cityID,
+                                Double.parseDouble(firstDay.getTMax()));
+                    } else {
+                        System.out.println("No results for city " + cityID);
+                    }
 
-            } else {
-                System.out.println( "No results for this request!");
+                    TimeUnit.SECONDS.sleep(20); // Aguarde 20 segundos antes de fazer a próxima solicitação
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-
-            System.exit(0);
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
-
     }
-}
+
